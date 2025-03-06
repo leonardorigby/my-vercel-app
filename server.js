a5498217e4be1482fecd3357e65d8b3f550b3088
lrcdeveloper <leorigbyc@gmail.com>
2025-03-06 13:14:47 -0600

    feat: live reaload

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
-const app = express();
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
 
+const app = express();
 const PORT = 3000;
 
-// Serve static files from the current directory
+// Create LiveReload server
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(__dirname);
+
+// Inject LiveReload script into the browser
+app.use(connectLivereload());
+
+// Serve static files
 app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+// Reload when files change
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
 });
